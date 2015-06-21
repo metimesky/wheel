95e085604d1b2f0dbe04bf41fcecb9ca2508e4b9
songziming <s.ziming@hotmail.com>
2015-06-22 03:00:32 +0800

    removed comments and other debugging stff in main.c

--- kernel/main.c
+++ kernel/main.c
@@ -25,28 +25,12 @@ void read_info(uint32_t eax, uint32_t ebx) {
 
     // are `mem_*` and `mmap_*` valid?
     if (mbi->flags & (1<<0) && mbi->flags & (1<<6)) {
+        // check lower and upper memory bounds
         raw_write("lower memory (KB):", 0x0f, 80*line);
         raw_write(u32_to_str(mbi->mem_lower, buf, 10), 0x0f, 80*line + 19);
         raw_write("upper memory (KB):", 0x0f, 80*line + 30);
         raw_write(u32_to_str(mbi->mem_upper, buf, 10), 0x0f, 80*line + 49);
         ++line;
-
-        /*
-        multiboot_memory_map_t *mmap = (multiboot_memory_map_t *) mbi->mmap_addr;
-        while ((uint32_t) mmap < mbi->mmap_addr + mbi->mmap_length) {
-            raw_write("Addr:", 0x0d, 80*line);
-            raw_write(u64_to_str(mmap->addr, buf, 16), 0x0d, 80*line+6);
-            raw_write("Size:", 0x0d, 80*line+30);
-            raw_write(u64_to_str(mmap->len, buf, 16), 0x0d, 80*line+36);
-            if (MULTIBOOT_MEMORY_AVAILABLE == mmap->type) {
-                raw_write("OK", 0x0a, 80*line+78);
-            } else {
-                raw_write("NO", 0x0c, 80*line+78);
-            }
-            mmap = (multiboot_memory_map_t *)((uint32_t) mmap + mmap->size + sizeof(uint32_t));
-            ++line;
-        }
-        */
 
         // init page allocator
         page_alloc_init(mbi->mmap_addr, mbi->mmap_length);
@@ -73,59 +57,4 @@ extern uint64_t buddy_num[8];
 
 void wheel_main(uint32_t eax, uint32_t ebx) {
     read_info(eax, ebx);
-    char buf[128];
-    uint64_t bitmap;
-    bitmap_zero(&bitmap, 64);
-    raw_write(u64_to_str(bitmap, buf, 2), 0x0f, 80*line); ++line;
-    BIT_SET(&bitmap, 5);
-    raw_write(u64_to_str(bitmap, buf, 2), 0x0f, 80*line); ++line;
-    BIT_SET(&bitmap, 15);
-    raw_write(u64_to_str(bitmap, buf, 2), 0x0f, 80*line); ++line;
-    bitmap_fill(&bitmap, 64);
-    raw_write(u64_to_str(bitmap, buf, 2), 0x0f, 80*line); ++line;
-    BIT_CLEAR(&bitmap, 5);
-    raw_write(u64_to_str(bitmap, buf, 2), 0x0f, 80*line); ++line;
-    BIT_CLEAR(&bitmap, 15);
-    raw_write(u64_to_str(bitmap, buf, 2), 0x0f, 80*line); ++line;
-    raw_write(u64_to_str(BIT_TEST(&bitmap, 3), buf, 2), 0x0f, 80*line); ++line;
-    raw_write(u64_to_str(BIT_TEST(&bitmap, 15), buf, 2), 0x0f, 80*line); ++line;
-/*
-    // print out symbols defined in linker script   
-    raw_write(u64_to_str((uint64_t)&kernel_load_addr, buf, 16), 0x1e, 0);
-    raw_write(u64_to_str((uint64_t)&kernel_end, buf, 16), 0x1e, 80);
-    raw_write(u64_to_str((uint64_t)&kernel_text_start, buf, 16), 0x1e, 160);
-    raw_write(u64_to_str((uint64_t)&kernel_text_end, buf, 16), 0x1e, 240);
-    raw_write(u64_to_str((uint64_t)&kernel_data_start, buf, 16), 0x1e, 320);
-    raw_write(u64_to_str((uint64_t)&kernel_data_end, buf, 16), 0x1e, 400);
-    raw_write(u64_to_str((uint64_t)&kernel_bss_start, buf, 16), 0x1e, 480);
-    raw_write(u64_to_str((uint64_t)&kernel_bss_end, buf, 16), 0x1e, 560);
-*/
-/*
-    for (int i = 0; i < 8; ++i) {
-        raw_write(u64_to_str((uint64_t) buddy_map[i], buf, 16), 0x0b, 80*line);
-        raw_write(u64_to_str((uint64_t) buddy_num[i], buf, 10), 0x0b, 80*line+40);
-        ++line;
-    }
-*/
-    uint64_t addr;
-    raw_write(u64_to_str(addr = alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(1), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    free_pages(addr, 0);
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    
-//    raw_write(u64_to_str(find_free_pages(0), buf, 16), 0x0b, 80*line); ++line;
-/*    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;
-    raw_write(u64_to_str(alloc_pages(0), buf, 16), 0x0b, 80*line); ++line;*/
 }
