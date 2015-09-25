67ebf9b3a858d87c8774331d2208e47b178f0136
songziming <s.ziming@hotmail.com>
2015-09-25 21:19:34 +0800

    turns out that's because of the video ram overflow

--- acpi/acpi.c
+++ acpi/acpi.c
@@ -80,15 +80,20 @@ void process_madt(madt_t *madt) {
     } else {
         println(" without dual 8259 legacy.");
     }
+    // return;
     uint8_t *p = madt->entries;
-    while (p < (uint8_t *) madt + madt->header.length) {
+    int i = 0;
+    while (p < (uint8_t *) madt + madt->header.length/* && i < 30*/) {
         switch (*p) {
         case 0: {   // processor local APIC
             madt_local_apic_entry_t *entry = (madt_local_apic_entry_t *) p;
-            println("processor local APIC");
-            // print("ACPI processor id ");
+            // print("processor local APIC, length=");
+            // print(u32_to_str(entry->length, buf, 10));
+            // print(" and size=");
+            // print(u32_to_str(sizeof(madt_local_apic_entry_t), buf, 10));
+            // print(". processorId ");
             // print(u32_to_str(entry->processor_id, buf, 10));
-            // print(" local APIC id ");
+            // print(" APICId ");
             // print(u32_to_str(entry->local_apic_id, buf, 10));
             // print(" flag ");
             // println(u32_to_str(entry->flags, buf, 10));
@@ -97,19 +102,22 @@ void process_madt(madt_t *madt) {
             break;
         case 1: {   // IO APIC
             madt_io_apic_entry_t *entry = (madt_io_apic_entry_t *) p;
-            println("IO APIC");
-            // print("IO APIC id ");
+            // print("IO APIC, length=");
+            // print(u32_to_str(entry->length, buf, 10));
+            // print(" and size=");
+            // print(u32_to_str(sizeof(madt_io_apic_entry_t), buf, 10));
+            // print(". APIC id=");
             // print(u32_to_str(entry->io_apic_id, buf, 10));
-            // print(" addr 0x");
+            // print(" addr=0x");
             // print(u32_to_str(entry->io_apic_addr, buf, 16));
-            // print(" interrupt base 0x");
+            // print(" int_base=0x");
             // println(u32_to_str(entry->global_system_interrupt_base, buf, 16));
             p += entry->length;
         }
             break;
         case 2: {   // interrupt source override
             madt_interrupt_src_override_t *entry = (madt_interrupt_src_override_t *) p;
-            println("interrupt source override");
+            // println("interrupt source override");
             // print("int from bus ");
             // print(u32_to_str(entry->bus_source, buf, 10));
             // print(" irq ");
@@ -120,10 +128,11 @@ void process_madt(madt_t *madt) {
         }
             break;
         default:
-            println("unknown MADT entry type!");
+            // println("unknown MADT entry type!");
             ++p;
             break;
         }
+        ++i;
     }
 }
 
@@ -132,50 +141,78 @@ void process_madt(madt_t *madt) {
 void process_acpi_table(uint64_t addr) {
     if (memcmp((char *) addr, "APIC", 4) == 0) {
         // multiple APIC description table
+        // println("APIC");
         process_madt((madt_t *) addr);
     } else if (memcmp((char *) addr, "BERT", 4) == 0) {
         // boot error record table
+        println("BERT");
     } else if (memcmp((char *) addr, "CPEP", 4) == 0) {
         // corrected platform error polling table
+        println("CPEP");
     } else if (memcmp((char *) addr, "DSDT", 4) == 0) {
         // differentiated system description table
+        println("DSDT");
     } else if (memcmp((char *) addr, "ECDT", 4) == 0) {
         // embedded controller boot resources table
+        println("ECDT");
     } else if (memcmp((char *) addr, "EINJ", 4) == 0) {
         // error injection table
+        println("EINJ");
     } else if (memcmp((char *) addr, "ERST", 4) == 0) {
         // error record serialization table
+        println("ERST");
     } else if (memcmp((char *) addr, "FACP", 4) == 0) {
-        // fixed APIC description table
+        // fixed ACPI description table
+        println("FACP");
     } else if (memcmp((char *) addr, "FACS", 4) == 0) {
         // firmware ACPPI control structure
+        println("FACS");
     } else if (memcmp((char *) addr, "HEST", 4) == 0) {
         // hardware error source table
+        println("HEST");
     } else if (memcmp((char *) addr, "MSCT", 4) == 0) {
         // maximum system characteristics table
+        println("MSCT");
     } else if (memcmp((char *) addr, "MPST", 4) == 0) {
         // memory power state table
+        println("MPST");
     } else if (memcmp((char *) addr, "OEM", 3) == 0) {
         // OEM specific information tables
         // there is not one table, but many tables!
+        println("OEM");
     } else if (memcmp((char *) addr, "PMTT", 4) == 0) {
         // platform memory topology table
+        println("PMTT");
     } else if (memcmp((char *) addr, "PSDT", 4) == 0) {
         // persistent system description table
+        println("PSDT");
     } else if (memcmp((char *) addr, "RASF", 4) == 0) {
         // ACPI RAS feature table
+        println("RASF");
     } else if (memcmp((char *) addr, "RSDT", 4) == 0) {
         // RSDT, already processed
+        println("RSDT");
     } else if (memcmp((char *) addr, "SBST", 4) == 0) {
         // smart battery specification table
+        println("SBST");
     } else if (memcmp((char *) addr, "SLIT", 4) == 0) {
         // System Locality System Information Table
+        println("SLIT");
     } else if (memcmp((char *) addr, "SRAT", 4) == 0) {
         // System Resource Affinity Table
+        println("SRAT");
     } else if (memcmp((char *) addr, "SSDT", 4) == 0) {
         // Secondary System Description Table
+        println("SSDT");
     } else if (memcmp((char *) addr, "XSDT", 4) == 0) {
         // XSDT, already processed
+        println("XSDT");
+    } else {
+        print("Others ");
+        char buf[5];
+        memcpy(buf, (char *) addr, 4);
+        buf[4] = '\0';
+        println(buf);
     }
 }
 
@@ -207,6 +244,8 @@ void acpi_init() {
         for (int i = 0; i < sdt_num; ++i) {
             process_acpi_table(rsdt->sdt_entries[i]);
         }
+        print("done");
+        println(u32_to_str(sdt_num, buf, 10));
     } else {
         // newer than 1.0, treat them as 2.0
         rsdp_2_t *rsdp = (rsdp_2_t *) addr;
@@ -225,5 +264,6 @@ void acpi_init() {
         for (int i = 0; i < sdt_num; ++i) {
             process_acpi_table(xsdt->sdt_entries[i]);
         }
+        println("done");
     }
 }
--- library/string.c
+++ library/string.c
@@ -37,12 +37,11 @@ void memset(void *dst, char c, int n) {
 // }
 
 int memcmp(const void *s1, const void *s2, int n) {
-    unsigned char u1, u2;
-    for (; n--; s1++, s2++) {
-        u1 = * (unsigned char *) s1;
-        u2 = * (unsigned char *) s2;
-        if ( u1 != u2) {
-            return (u1-u2);
+    unsigned char *u1 = (const unsigned char *) s1;
+    unsigned char *u2 = (const unsigned char *) s2;
+    for (; n--; u1++, u2++) {
+        if (*u1 != *u2) {
+            return (*u1 - *u2);
         }
     }
     return 0;
