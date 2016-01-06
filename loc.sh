73a001bd1d18ae80ac3196d6a7997566a8ad693c
songziming <s.ziming@hotmail.com>
2016-01-07 03:53:59 +0800

    changed loc.sh to exclude ACPICA

--- loc.sh
+++ loc.sh
@@ -2,6 +2,7 @@
 # count the lines of code
 
 find . \
-    -not \( -path "./build/*" -prune \)        \
-    -name \*.asm -o -name \*.c -o -name \*.h   \
+    -not \( -path "./build/" -prune \)                      \
+    -not \( -path "./kernel/drivers/acpi/acpica/" -prune \) \
+    -name \*.asm -o -name \*.c -o -name \*.h                \
     | xargs wc -l
