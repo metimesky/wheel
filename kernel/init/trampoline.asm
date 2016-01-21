e5dd936166eb88d770d35a450ef9c413f7ad32e0
songziming <s.ziming@hotmail.com>
2016-01-22 00:40:10 +0800

    need to modify kernel image section to make trampoline function

--- kernel/init/trampoline.asm
+++ kernel/init/trampoline.asm
@@ -4,9 +4,8 @@
 ; When switching to long mode, we can make use of the original code.
 
 global trampoline
-global trampoline_end
 
-[section .text]
+[section .tramp]
 
 [BITS 16]
 trampoline:
@@ -24,19 +23,48 @@ trampoline:
     mov     al, 'o'
     mov     [gs:18], ax
 
-    jmp     $
-
     ; AP is different with BSP, we don't need to enable A20
-    mov     eax, real_trampoline
-    push    0
-;    jmp far real_trampoline     ; use far jump, avoid relative addressing
+    ;lgdt    [tmp_gdt_ptr]
+    cli
+    mov     eax, cr0
+    or      eax, 1
+    mov     cr0, eax
+    ;jmp     tmp_gdt.code:pm_trampoline
 
     jmp     $
-    jmp     $
-trampoline_end:
 
+tmp_gdt:
+.null   equ $ - tmp_gdt
+    dd      0
+    dd      0
+.code   equ $ - tmp_gdt
+    dw      0               ; limit [0:15]
+    dw      0               ; base [0:15]
+    db      0               ; base [16:23]
+    db      10011000b       ; Present, DPL=0, non-conforming
+    db      10100000b       ; 64-bit, and limit[16:19]
+    db      0               ; base [24:31]
+.data   equ $ - tmp_gdt
+    dw      0               ; limit [0:15]
+    dw      0               ; base [0:15]
+    db      0               ; base [16:23]
+    db      10010010b       ; Present, DPL=0, writable
+    db      11000000b       ; limit [16:19] and attr
+    db      0               ; base [24:31]
 
-real_trampoline:
+tmp_gdt_ptr:
+    dw      $ - tmp_gdt - 1 ; limit
+    dd      tmp_gdt         ; base
+
+[BITS 32]
+pm_trampoline:
+    mov     ax, 0x10
+    mov     ds, ax
+    mov     es, ax
+    mov     fs, ax
+    mov     gs, ax
+    mov     ss, ax
+
     mov     ax, 0xb800
     mov     gs, ax
     mov     ah, 0x0c
